63df8505ef042da97bf8c051447e1e6efcbf495b
“Christopher <2834237140101@ingenieria.usac.edu.gt>
2023-06-21 00:59:42 -0600

    [ADD] validacion nombre y clave, que venga palabra credenciales

--- prac.asm
+++ prac.asm
@@ -5,6 +5,7 @@
 .DATA
 ;; VARIABLES
 nueva_linea  db      0a,"$"
+contador_global dw  0
     ;; ENCABEZADO
 usac        db      "Universidad de San Carlos de Guatemala",0a,"$" ;; 0a -> nueva linea
 facultad    db      "Facultad de Ingenieria",0a,"$"
@@ -13,31 +14,39 @@ curso       db      "Arquitectura de Computadoras y Ensambladores 1",0a,"$"
 nombre      db      "Nombre: Christopher Alexander Acajabon Gudiel",0a,"$"
 carnet      db      "Carnet: 201404278",0a,"$"
     ;; ACCESO
-archivo_acceso      db  "PRA2.CNF",00 ;; 00 -> caracter nulo, lo pide el archivo (cadena ASCIZ)
-error_acceso        db  "Archivo no encontrado o error de credenciales",0a,"$"
-handle_acceso       dw  0000 ;; para guardar puntero archivo abierto
-buffer_acceso_leido db  43  dup (0),"$" ; 57d $ -> indica cuando voy a parar
-nombre_acceso       db  "cgudiel"
-clave_acceso        db  "201404278"
-buffer_leido_actual db  43   dup (0) ; 57d, buffer donde estara el nombre y password extraidos del archivo
+archivo_acceso          db  "PRA2.CNF",00 ;; 00 -> caracter nulo, lo pide el archivo (cadena ASCIZ)
+str_error_acceso_arch   db  "Archivo no encontrado",0a,"$"
+str_error_acceso_cont   db  "Error estructura o credenciales",0a,"$"
+handle_acceso           dw  0000 ;; para guardar puntero archivo abierto
+buffer_acceso_leido     db  43  dup (0),"$" ; 57d $ -> indica cuando voy a parar
+str_credenciales        db  "credenciales"
+str_nombre_acceso       db  "cgudiel"
+str_clave_acceso        db  "201404278"
+buffer_leido_actual     db  43   dup (0) ; 57d, buffer donde estara el nombre y password extraidos del archivo
 contador_buffer_bia     dw  0 ; para limpiar el buffer_leido_actual
-contador_actual_comilla  dw 0 ; indice para saber desde donde voy a ler del buffer_acceso_leido
+cont_actual_caracer     dw 0 ; indice para saber desde donde voy a ler del buffer_acceso_leido
 comillas            db '"'
+corchete_abre       db '['
+corchete_cierre     db ']'
 
 .CODE
 .STARTUP
 ;; CODIGO
 inicio:
 
+;; ---------------------------------------------------------- ACCESO ------------------------------------------------------------------
 call acceso
 
 acceso proc
+
+    call encabezado
+
         ;; abrir archivo
     mov al, 02
     mov dx, offset archivo_acceso
     mov ah, 3d
     int 21
-    jc eti_error_acceso ; si cf = 1 hay error
+    jc eti_error_archivo_acceso ; si cf = 1 hay error
     mov [handle_acceso], ax ;; guardamos handles del archivo abierto
 
         ;; lectura de archivo
@@ -46,34 +55,67 @@ acceso proc
     mov dx, offset buffer_acceso_leido
     mov ah, 3f
     int 21
-    jc eti_error_acceso
+    jc eti_error_archivo_acceso
 
-    mov contador_actual_comilla, 0
+        ;; extraemos parte de [credenciales]
+    mov cont_actual_caracer, 0
     call limpiar_buffer_leido_actual
-    call extraer_contenido_comillas
-
-    mov dx, offset buffer_leido_actual
-    mov ah, 09
-    int 21
-
-    inc contador_actual_comilla
-    call limpiar_buffer_leido_actual
-    call extraer_contenido_comillas
-
-    mov dx, offset buffer_leido_actual
-    mov ah, 09
-    int 21
-
-
-        ; imprimir archivo
-    ; call imprimir_estructura
-
-        ;; cerrar archivo, bx ya esta en handle_acceso
-    ; mov bx, [handle_acceso]
-    ; mov ah, 3e
+    mov di, offset corchete_abre
+    mov si, offset corchete_cierre
+    call extraer_contenido_entre_caracteres
+        ;; comparamos nombre credenciales
+    mov si, offset buffer_leido_actual
+    mov di, offset str_credenciales
+    mov cx, 000ch ;; 12d, cantidad de caracteres a comparar
+    call comparar_cadenas
+    cmp dl, 0ff
+    jne eti_error_cred_acceso
+        ;; imprimir literal 'credenciales'
+    ; mov dx, offset buffer_leido_actual
+    ; mov ah, 09
     ; int 21
 
-    call encabezado
+        ;; extraemos nombre
+    inc cont_actual_caracer
+    call limpiar_buffer_leido_actual
+    mov di, offset comillas
+    mov si, offset comillas
+    call extraer_contenido_entre_caracteres
+        ;; comparamos nombre
+    mov si, offset buffer_leido_actual
+    mov di, offset str_nombre_acceso
+    mov cx, 0007 ;; cantidad de caracteres a comparar
+    call comparar_cadenas
+    cmp dl, 0ff
+    jne eti_error_cred_acceso
+        ;; imprimir nombre
+    ; mov dx, offset buffer_leido_actual
+    ; mov ah, 09
+    ; int 21
+
+        ;; extraemos clave
+    inc cont_actual_caracer
+    call limpiar_buffer_leido_actual
+    mov di, offset comillas
+    mov si, offset comillas
+    call extraer_contenido_entre_caracteres
+        ;; comparamos clave
+    mov si, offset buffer_leido_actual
+    mov di, offset str_clave_acceso
+    mov cx, 0009
+    call comparar_cadenas
+    cmp dl, 0ff
+    jne eti_error_cred_acceso
+        ;; imprimir clave
+    ; mov dx, offset buffer_leido_actual
+    ; mov ah, 09
+    ; int 21
+
+        ;; cerrar archivo, bx esta el handle_acceso
+    mov bx, [handle_acceso]
+    mov ah, 3e
+    int 21
+
     jmp fin    
 acceso endp
 
@@ -90,29 +132,31 @@ limpiar_buffer_leido_actual proc
     ret
 limpiar_buffer_leido_actual endp
 
-extraer_contenido_comillas proc
-
+extraer_contenido_entre_caracteres proc
+    ;; ENTRADA -> "nombre" -> para extraer 'nombre' primer caracter es ", segundo caracter es "
+        ;; DI -> primer caracter
+        ;; SI -> segundo caracter
     buscar_comilla:
-        mov bx, contador_actual_comilla
+        mov bx, cont_actual_caracer
         mov al, buffer_acceso_leido[bx]
-        cmp al, comillas
-        jne siguiente_caracter ;; si no es comilla avanza al siguiente caracer
-        inc contador_actual_comilla ;; si llega aca, encontro comilla, pasa al metodo de abajo
+        cmp al, [di] ;; caracter donde empezar la extraccion
+        jne siguiente_caracter ;; si no es caracter donde empezar extraccion avanza al siguiente caracer
+        inc cont_actual_caracer ;; si llega aca, encontro comilla, pasa al metodo de abajo
 
-    guardar_caracteres_entre_comillas:
-        mov bx, contador_actual_comilla
+    guardar_caracter_extraido:
+        mov bx, cont_actual_caracer
         mov al, buffer_acceso_leido[bx]
-        cmp al, comillas
-        je fin_extraccion ;; si es comilla, fin extraccion
+        cmp al, [si] ;; caracter donde termina la extraccion
+        je fin_extraccion ;; si es caracter donde termina extraccion, fin extraccion
         mov bx, contador_buffer_bia
         mov buffer_leido_actual[bx], al ;; guardamos caracter
         inc contador_buffer_bia
-        inc contador_actual_comilla
-        jmp guardar_caracteres_entre_comillas ;; avanza al siguiente caracter
+        inc cont_actual_caracer
+        jmp guardar_caracter_extraido ;; avanza al siguiente caracter
 
     siguiente_caracter:
-        inc contador_actual_comilla
-        mov bx, contador_actual_comilla
+        inc cont_actual_caracer
+        mov bx, cont_actual_caracer
         cmp buffer_acceso_leido[bx], 24 ; $ hex h ***********
         jne buscar_comilla ; si no es fin de cadena, siguiente caracter
 
@@ -121,8 +165,7 @@ extraer_contenido_comillas proc
         mov buffer_leido_actual[bx], 24 ; $ hex
         ret
 
-extraer_contenido_comillas endp
-
+extraer_contenido_entre_caracteres endp
 
 imprimir_estructura proc
     mov DI, offset buffer_acceso_leido
@@ -145,11 +188,19 @@ imprimir_estructura proc
             ret    
     imprimir_estructura endp
 
-    eti_error_acceso:
-        mov dx, offset error_acceso
-        mov ah, 09
-        int 21
-        jmp fin
+eti_error_archivo_acceso:
+    mov dx, offset str_error_acceso_arch
+    mov ah, 09
+    int 21
+    jmp fin
+
+eti_error_cred_acceso:
+    mov dx, offset str_error_acceso_cont
+    mov ah, 09
+    int 21
+    jmp fin
+
+;; ---------------------------------------------------------- ENCABEZADO ------------------------------------------------------------------
 
     ;; ENCABEZADO
 encabezado proc
@@ -176,6 +227,30 @@ encabezado proc
     int 21
     ret    
 encabezado endp
+
+comparar_cadenas proc
+    ;; ENTRADA
+        ;; SI -> direccion de cadena 1
+        ;; DI -> direccion de cadena 2
+        ;; CX -> tamano maximo
+    ;; SALIDA
+        ;; DL -> 00 si no son iguales
+        ;;    -> 0ff si son iguales
+
+    ciclo_cadenas_iguales:
+        mov al, [si]
+        cmp [di], al
+        jne no_iguales
+        inc di
+        inc si
+        loop ciclo_cadenas_iguales ;; el loop baja en 1 automaticamente, segun lo que tenga cx
+        mov dl, 0ff ;; son iguales
+        ret
+    no_iguales:
+        mov dl, 00 ;; no son iguales
+        ret
+comparar_cadenas endp
+
 fin:
 .EXIT
 END
